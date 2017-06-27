4549f3d596913b4a763d553e234d03c776cf49cc
abernardo <abernardo@wfgmb.com>
2017-06-27 19:40:43 +0800

    Update.

--- src/test-api.js
+++ src/test-api.js
@@ -38,86 +38,86 @@ countries.forEach((data) => {
     countriesByName.set(data.name, [data.latitude, data.longitude]);
 });
 
-const IvoryCoast = 
+const IvoryCoast =
 
 //Using import/export
-let imp1 = new ImportTransaction({
-    modeOfTransport: new ByAir({
-        // polyline: "none",
-        marker: {durations: 15000}
-    }),
-    from: IvoryCoast,
-    to: Philippines
-});
+// let imp1 = new ImportTransaction({
+//     modeOfTransport: new ByAir({
+//         // polyline: "none",
+//         marker: {durations: 15000}
+//     }),
+//     from: IvoryCoast,
+//     to: Philippines
+// });
 
-imp1.addTo(mymap);
-imp1.animate();
-
-let imp2 = new ImportTransaction({
-    modeOfTransport: new ByAir({
-        marker: {
-            durations: 3000
-        },
-        // polyline: "none"
-    }),
-    from: IvoryCoast,
-    to: Brazil
-});
-
-imp2.addTo(mymap).animate();
-
-let imp3 = new ImportTransaction({
-    modeOfTransport: new ByAir({
-        marker: {durations: 16000},
-        // polyline: "none"
-    }),
-    from: IvoryCoast,
-    to: Japan
-});
-
-imp3.addTo(mymap).animate();
-
-let exp = new ExportTransaction({
-    modeOfTransport: new ByAir({
-        marker: {durations: 11000},
-        // polyline: "none"
-    }),
-    from: IvoryCoast,
-    to: somePlace
-}).addTo(mymap).animate();
-
-let exp2 = new ExportTransaction({
-    modeOfTransport: new ByAir({
-        marker: {
-            durations: 7000,
-            // options: {
-            //     icon: L.icon({
-            //         // iconUrl: "https://image.flaticon.com/icons/svg/60/60517.svg",
-            //         // iconSize: [40,40],
-            //         className: "blinking",
-            //     })
-            // }
-        },
-        // polyline: "none"
-    }),
-    from: IvoryCoast,
-    to: US
-}).addTo(mymap).animate();
-
-let exp3 = new ExportTransaction({
-    modeOfTransport: new ByAir({
-        // marker: {durations: 10000},
-        // polyline: "none"
-    }),
-    from: IvoryCoast,
-    to: Canada
-}).addTo(mymap).animate();
-
-let expShip = new ExportTransaction({
-    modeOfTransport: new BySea({
-        // polyline: "none",
-        marker: {durations: 15000}
-    }),
-    from: IvoryCoast,
-    to: Angola
-}).addTo(mymap).animate();
+// imp1.addTo(mymap);
+// imp1.animate();
+//
+// let imp2 = new ImportTransaction({
+//     modeOfTransport: new ByAir({
+//         marker: {
+//             durations: 3000
+//         },
+//         // polyline: "none"
+//     }),
+//     from: IvoryCoast,
+//     to: Brazil
+// });
+//
+// imp2.addTo(mymap).animate();
+//
+// let imp3 = new ImportTransaction({
+//     modeOfTransport: new ByAir({
+//         marker: {durations: 16000},
+//         // polyline: "none"
+//     }),
+//     from: IvoryCoast,
+//     to: Japan
+// });
+//
+// imp3.addTo(mymap).animate();
+//
+// let exp = new ExportTransaction({
+//     modeOfTransport: new ByAir({
+//         marker: {durations: 11000},
+//         // polyline: "none"
+//     }),
+//     from: IvoryCoast,
+//     to: somePlace
+// }).addTo(mymap).animate();
+//
+// let exp2 = new ExportTransaction({
+//     modeOfTransport: new ByAir({
+//         marker: {
+//             durations: 7000,
+//             // options: {
+//             //     icon: L.icon({
+//             //         // iconUrl: "https://image.flaticon.com/icons/svg/60/60517.svg",
+//             //         // iconSize: [40,40],
+//             //         className: "blinking",
+//             //     })
+//             // }
+//         },
+//         // polyline: "none"
+//     }),
+//     from: IvoryCoast,
+//     to: US
+// }).addTo(mymap).animate();
+//
+// let exp3 = new ExportTransaction({
+//     modeOfTransport: new ByAir({
+//         // marker: {durations: 10000},
+//         // polyline: "none"
+//     }),
+//     from: IvoryCoast,
+//     to: Canada
+// }).addTo(mymap).animate();
+//
+// let expShip = new ExportTransaction({
+//     modeOfTransport: new BySea({
+//         // polyline: "none",
+//         marker: {durations: 15000}
+//     }),
+//     from: IvoryCoast,
+//     to: Angola
+// }).addTo(mymap).animate();
